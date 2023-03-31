2b4f2bcee12cd6995b8d666d08cda01d663001d8
nadarvishal <116131766+nadarvishal@users.noreply.github.com>
2023-03-31 04:03:45 -0400

    Update index.js

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -4,11 +4,12 @@ import { useEffect, useRef } from 'react'
 
 
 
+
 const Logo = () => {
     const bgRef = useRef()
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
-
+    
     return (
         <div className="logo-container" ref={bgRef}>
             <img className="solid-logo" ref={solidLogoRef} src={LogoS} alt="S" />
