c2efcc73456f2ed14d0763a5846f06fb5ad5c9e9
Vishal <vnadar@gmu.edu>
2023-03-31 01:49:45 -0400

    setup gh-pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -29,18 +29,16 @@ const About = () => {
                 />
               </h1>
               <p>
-                I'm a very ambitious front-end developer looking for a role in an
-                established IT company with the opportunity to work with the latest
-                technologies on challenging and diverse projects.
+              I am currently in the final semester of my Master of Science in Computer Science from George Mason University with a GPA of 3.7/4. During my time at George Mason University, I gained hands-on experience in various programming languages, web technologies, and software. My coursework and research experience have allowed me to hone my skills in Python, Java, HTML, CSS, JavaScript, AngularJS, ReactJS, and Spring Boot. I am confident that my skills and experience make me a strong candidate for this position.
               </p>
               <p align="LEFT">
-                I'm quiet confident, naturally curious, and perpetually working on
-                improving my chops one design problem at a time.
+              As a Web Developer at George Mason University, I updated the university's faculty webpage using React.js. This experience helped me develop my skills in DOM, React Props, Lifecycle methods, working with React States, and Events. As a Graduate Research Assistant, I researched reasons for the increase in cyber-attacks due to COVID-19. My experience as a Teaching Assistant in Python, SysMl, and Data Structures and Algorithm has helped me develop my ability to grade assignments, provide feedback, and assist students with their questions.
               </p>
               <p>
-                If I need to define myself in one sentence that would be a family
-                person, father of a beautiful daughter, a sports fanatic,
-                photography enthusiast, and tech-obsessed!!!
+              My experience as a Software Development Engineer at GAMIC SOLUTIONS PRIVATE LIMITED helped me develop my skills in analysis, design, implementation, and testing phases of the Software Development Life Cycle (SDLC). I have experience working with Java 8 & 11 features like Stream API, Time API, Functional Interfaces Multithreading, Transaction Management, Exception Handling, and Collection API. I have also implemented Microservices-based architecture using Spring Boot interacting through REST API. This experience helped me develop my skills in Angular 2 framework, HTML5, CSS3, JavaScript, Bootstrap, Node.js, and Mongo DB.
+              </p>
+              <p>
+              I am confident that my skills and experience make me a strong candidate at your company. Thank you for considering my application. I look forward to the opportunity to discuss my qualifications in further detail.
               </p>
             </div>
         </div>
